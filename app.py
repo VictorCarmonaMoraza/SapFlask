2799ae4416065ac3803de66d2f94a4418ec7b93f
Victor <VictorCarmona9035@hotmail.com>
2023-05-16 22:49:16 +0200

    3-Flask.Migrate Alembic y SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 #Configuramos nuestra conexion a base de datos
 USER_DB = 'postgres'
-PASS_DB ='admin'
+PASS_DB ='Vcarmona32'
 URL_DB = 'localhost'
 NAME_DB = 'sap_flask_db_mayo_2023'
 FULL_URL_DB = f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
@@ -18,3 +19,23 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #Incicializacion del objeto db de sqlalchemy
 db = SQLAlchemy(app)
+
+#Configurar flask-migrate
+migrate =Migrate()
+migrate.init_app(app, db)
+
+#Esto va a representar kla tabla en base de datos con sus columnas
+class Persona(db.Model):
+    #atributos
+    id =db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(250))
+    apellido = db.Column(db.String(250))
+    email = db.Column(db.String(250))
+
+    def __str__(self):
+        return (
+            f'Id: {self.id}, '
+            f'Nombre: {self.nombre}, '
+            f'Apellido: {self.apellido}, '
+            f'Email: {self.email}'
+        )
